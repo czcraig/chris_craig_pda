39f966b99edb2cf418cbb99c6f469024c4fcb2f5
Chris Craig <ctc2@live.co.uk>
2018-09-19 00:34:16 +0100

    test created

---
+++ pda_evidence/sorting_data.rb
@@ -0,0 +1,7 @@
+array_of_teams = ["Arsenal", "Celtic", "Barcelona", "PSG", "Chelsea"]
+
+def sort_array_of_teams(array)
+  return array.sort
+end
+
+p sort_array_of_teams(array_of_teams)
---
+++ pda_evidence/specs/cardgame_spec.rb
@@ -0,0 +1,28 @@
+require("minitest/autorun")
+require_relative("../testing_task_2.rb")
+
+
+class TestCardGame < MiniTest::Test
+
+def setup
+    @card1 = Card.new("Spades", 1)
+    @card2 = Card.new("Hearts", 7)
+    @cards = [@card1, @card2]
+end
+
+def test_checkforAce
+  result = CardGame.checkforAce(@card1)
+  assert_equal(true, result)
+end
+
+def test_highest_card
+  result = CardGame.highest_card(@card1, @card2)
+  assert_equal(@card2, result)
+end
+
+def test_cards_total
+  result = (CardGame.cards_total(@cards))
+  assert_equal( "You have a total of 8", result)
+end
+
+end
