11d82d8ce7f6b3b11c0fbc7250157879e9d9780e
Chris Craig <ctc2@live.co.uk>
2018-10-04 02:08:23 +0100

    first test passed

---
+++ pda_evidence/Coding exercises/Static_and_Dynamic_Task_A/specs/cardgame_spec.rb
@@ -0,0 +1,20 @@
+
+require("minitest/autorun")
+require_relative("../testing_task_2.rb")
+
+
+class TestCardGame < MiniTest::Test
+
+def setup
+    @card1 = Card.new("Diamonds", 7)
+    @card2 = Card.new("Spades", 1)
+    @cards = [@card1, @card2]
+end
+
+def test_checkforAce
+    result = CardGame.check_for_ace(@card2)
+    assert_equal(true, result)
+end
+
+
+end
