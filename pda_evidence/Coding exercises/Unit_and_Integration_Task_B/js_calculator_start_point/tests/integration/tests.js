9c2d58f90b155a0129c9d638a6251fb522865b71
Chris Craig <ctc2@live.co.uk>
2018-10-15 00:40:21 +0100

    tests fixed

--- pda_evidence/Coding exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ pda_evidence/Coding exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -22,7 +22,7 @@ describe('calculator functionality', function() {
     element(by.css('#number2')).click();
     element(by.css('#operator_multiply')).click();
     element(by.css('#number4')).click();
-    element(by.css('#operator_multiply')).click();
+    element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('8')
   });
 
